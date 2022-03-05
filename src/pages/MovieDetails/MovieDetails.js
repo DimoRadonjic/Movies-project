402cc5bd28dd77fb30dfe4cbef46041df09876d7
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-03-05 01:59:46 +0100

    edit comment done

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -19,9 +19,11 @@ const MovieDetails = () => {
   const [edit, setEdit] = useState(false);
 
   const [newComment, setNewComment] = useState('');
+  const [count, setCount] = useState(0);
   const { loggedIn, cast, token, profile } = useContext(GlobalContext);
   let movieCast = cast.filter((cast) => cast.movie === movie_selected.id);
 
+  console.log('Movie', movie_selected);
   console.log('Profile', profile);
 
   useEffect(() => {
@@ -39,46 +41,44 @@ const MovieDetails = () => {
       );
       let comments = data;
       setComments(comments);
+      setCount(comments.length);
       console.log('Data', data);
     }
     fetchReviews();
   }, [movie_selected.id]);
+  console.log('Votes', comments.length);
 
   useEffect(() => {
     switch (rating) {
       case 1:
         setSum_Score(sum_score + 1);
-        movie_selected.count_score++;
 
         break;
       case 2:
         setSum_Score(sum_score + 2);
-        movie_selected.count_score++;
 
         break;
       case 3:
         setSum_Score(sum_score + 3);
-        movie_selected.count_score++;
 
         break;
       case 4:
         setSum_Score(sum_score + 4);
-        movie_selected.count_score++;
 
         break;
       case 5:
         setSum_Score(sum_score + 5);
-        movie_selected.count_score++;
 
         break;
 
       default:
         break;
     }
+    setCount(count + 1);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rating]);
-  console.log(Rating);
+
   const handleChangeRate = (e) => {
     e.preventDefault();
     setRating(parseInt(e.target.value));
@@ -101,12 +101,8 @@ const MovieDetails = () => {
   const handleClick = (e) => {
     axios
       .post(
-        'https://imdb-api.tk/api/reviews/',
-        {
-          movie: movie_selected.id,
-          comment: newComment,
-          rate: rating,
-        },
+        'https://imdb-api.tk//api/reviews/',
+        { comment: newComment, rate: rating, movie: movie_selected.id },
         {
           headers: {
             // Overwrite Axios's automatically set Content-Type
@@ -115,42 +111,78 @@ const MovieDetails = () => {
           },
         }
       )
-      .then(
-        () =>
-          axios.patch(`https://imdb-api.tk/api/movies/${movie_selected.id}/`),
-        {
-          runtime: movie_selected.runtime,
+      .then((data) => {
+        console.log('Data', data);
+        axios
+          .post(
+            'https://imdb-api.tk/api/reviews/',
+            {
+              movie: movie_selected.id,
+              comment: newComment,
+              rate: rating,
+            },
+            {
+              headers: {
+                // Overwrite Axios's automatically set Content-Type
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token.access}`,
+              },
+            }
+          )
+          .then(
+            () =>
+              axios.patch(
+                `https://imdb-api.tk/api/movies/${movie_selected.id}`
+              ),
+            {
+              runtime: movie_selected.runtime,
+              avg_score: sum_score / movie_selected.count_score,
+              count_score: count,
+            },
+            {
+              headers: {
+                // Overwrite Axios's automatically set Content-Type
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token.access}`,
+              },
+            }
+          )
+          .then((data) => console.log('Movie update', data));
+      });
 
-          avg_score: sum_score / movie_selected.count_score,
-          count_score: movie_selected.count_score,
-        },
-        {
-          headers: {
-            // Overwrite Axios's automatically set Content-Type
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token.access}`,
-          },
-        }
-      )
-      .then((data) => console.log('Movie update', data));
+    setNewComment('');
+    setRating(0);
   };
 
   const handleEdit = () => {
-    axios.patch(
-      `https://imdb-api.tk/api/reviews/${comments.id}/`,
-      {
-        rate: comments.rate,
-        comment: newComment,
-        movie: movie_selected.id,
-      },
-      {
-        headers: {
-          // Overwrite Axios's automatically set Content-Type
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token.access}`,
-        },
-      }
+    console.log('Comment new', newComment);
+    console.log('Comment', comments);
+    console.log(
+      'Comment id',
+      comments.find((comment) => comment.profile.id === profile.id)
     );
+
+    let updateComment = comments.find(
+      (comment) => comment.profile.id === profile.id
+    );
+
+    axios
+      .patch(
+        `https://imdb-api.tk/api/reviews/${updateComment.id}/`,
+        {
+          rate: updateComment.rate,
+          comment: newComment,
+          movie: updateComment.movie.id,
+        },
+        {
+          headers: {
+            // Overwrite Axios's automatically set Content-Type
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token.access}`,
+          },
+        }
+      )
+      .then((data) => console.log(data));
   };
 
   return (
@@ -181,11 +213,9 @@ const MovieDetails = () => {
               Genre: {movie_selected.genre.title}
             </div>
             <div style={{ marginTop: '2vh' }}>
-              Rating: {movie_selected.avg_score}
+              Rating: {parseFloat((sum_score / count).toFixed(1))}
             </div>
-            <div style={{ marginTop: '2vh' }}>
-              Votes: {movie_selected.count_score}
-            </div>
+            <div style={{ marginTop: '2vh' }}>Votes: {count}</div>
             <div style={{ marginTop: '2vh' }}>
               Cast:
               <ul style={{ listStyle: 'none' }}>
@@ -210,7 +240,8 @@ const MovieDetails = () => {
                     name='text-feedback'
                     value={rating}
                     precision={1}
-                    disabled={loggedIn === false ? false : true}
+                    disabled={loggedIn === false ? true : false}
+                    max={10}
                     emptyIcon={
                       <StarIcon
                         style={{ opacity: 0.45, color: 'yellow' }}
@@ -227,7 +258,7 @@ const MovieDetails = () => {
                 id='outlined-basic'
                 label='Comment'
                 multiline
-                disabled={loggedIn === false ? false : true}
+                disabled={loggedIn === false ? true : false}
                 variant='outlined'
                 style={{
                   background: 'white',
@@ -285,7 +316,7 @@ const MovieDetails = () => {
                                       borderRadius: '5px',
                                       width: '20vw',
                                     }}
-                                    // disabled={!loggedIn}
+                                    disabled={loggedIn === false ? true : false}
                                     onChange={(e) => handleUpdateComment(e)}
                                   />
                                 </div>
@@ -293,46 +324,50 @@ const MovieDetails = () => {
                                 <>{comment.comment}</>
                               )}
                               {comment.profile.id === profile.id ? (
-                                <div>
-                                  <Button
-                                    variant='contained'
-                                    style={{
-                                      marginTop: '1vw',
-                                      display: 'flex',
-                                      justifyContent: 'center',
-                                      alignItems: 'center',
-                                      color: 'black',
-                                      background: '#f6c900',
-                                      padding: '0',
-                                    }}
-                                    size='small'
-                                    onClick={() => {
-                                      setEdit(!edit);
-                                      handleEdit();
-                                    }}
-                                  >
-                                    Save
-                                  </Button>
-                                </div>
+                                edit === false ? (
+                                  <div>
+                                    <Button
+                                      variant='contained'
+                                      style={{
+                                        marginTop: '1vw',
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        color: 'black',
+                                        background: '#f6c900',
+                                        padding: '0.15rem',
+                                      }}
+                                      size='small'
+                                      onClick={() => setEdit(!edit)}
+                                    >
+                                      Edit Comment
+                                    </Button>
+                                  </div>
+                                ) : (
+                                  <div>
+                                    <Button
+                                      variant='contained'
+                                      style={{
+                                        marginTop: '1vw',
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        color: 'black',
+                                        background: '#f6c900',
+                                        padding: '0',
+                                      }}
+                                      size='small'
+                                      onClick={() => {
+                                        setEdit(!edit);
+                                        handleEdit();
+                                      }}
+                                    >
+                                      Save
+                                    </Button>
+                                  </div>
+                                )
                               ) : (
-                                <div>
-                                  <Button
-                                    variant='contained'
-                                    style={{
-                                      marginTop: '1vw',
-                                      display: 'flex',
-                                      justifyContent: 'center',
-                                      alignItems: 'center',
-                                      color: 'black',
-                                      background: '#f6c900',
-                                      padding: '0.15rem',
-                                    }}
-                                    size='small'
-                                    onClick={() => setEdit(!edit)}
-                                  >
-                                    Edit Comment
-                                  </Button>
-                                </div>
+                                ''
                               )}
                             </>
                           ) : (
@@ -353,6 +388,7 @@ const MovieDetails = () => {
                                   value={comment.rate}
                                   precision={1}
                                   disabled
+                                  max={10}
                                   emptyIcon={
                                     <StarIcon
                                       style={{
